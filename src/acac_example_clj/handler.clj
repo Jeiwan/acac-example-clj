b5e667ce09e8010976c15bb071e014d4ae0a7587
Ivan Kuznetsov <me@jeiwan.ru>
2017-11-09 09:12:48 +0700

    Handle requests to foo.example.dev and bar.example.dev

--- src/acac_example_clj/handler.clj
+++ src/acac_example_clj/handler.clj
@@ -3,9 +3,22 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(defroutes foo-routes
+  (GET "/" [] "Hello foo")
   (route/not-found "Not Found"))
+
+(defroutes bar-routes
+  (GET "/" [] "Hello bar")
+  (route/not-found "Not Found"))
+
+(defn domain-routing [domain-routes-map]
+  (fn [req]
+    (when-let [route (get domain-routes-map (get-in req [:headers "host"]))]
+      (route req))))
+
+(def app-routes
+  (domain-routing {"foo.example.dev:3000" foo-routes
+                   "bar.example.dev:3000" bar-routes}))
 
 (def app
   (wrap-defaults app-routes site-defaults))
